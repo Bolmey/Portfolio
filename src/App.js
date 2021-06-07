88db3b0a59abffd485d32cf6922b7181773e8cf3
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-06-07 16:16:21 -0400

    first section message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <p>Nothing going on here just hosting a website</p>
-      </header>
+      {/* ---------NAV BAR--------- */}
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Juan Bolmey</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="https://www.linkedin.com/in/bolmey/">Linkedin</Nav.Link>
+            <Nav.Link eventKey={2} href="https://github.com/Bolmey">
+              Github
+      </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {/* ---------NAV BAR END--------- */}
+      <section>
+        <div className="main-Header">
+          <div>
+            <h5>Hello</h5>
+            <h1>I'm Juan Bolmey</h1>
+            <h4>Web Developer</h4>
+          </div>
+          {/* IMG JUAN */}
+        </div>
+      </section>
+
     </div>
   );
 }
