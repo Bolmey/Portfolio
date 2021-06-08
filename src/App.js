31b62395b8e4d1d83d10b19d35be014a66e4dae5
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-06-08 12:10:38 -0400

    about me & Pro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import headerImg from './img/JuanPort.png';
+import aboutMeImg from './img/juanabout.png';
 
 
 function App() {
@@ -32,7 +32,7 @@ function App() {
             <h1>I'm Juan Bolmey</h1>
             <h4>Web Developer</h4>
           </div>
-          {/* IMG JUAN */}
+          <img className="headerImg" src={headerImg} />
         </div>
       </section>
 
@@ -41,44 +41,65 @@ function App() {
 
       {/* ---------ABOUT ME--------- */}
       <section className="aboutMe">
-        <img></img>
-        <div className="text-box">
+        <div className='aboutMeContainer'>
           <h2>About Me</h2>
           <div className="accent"></div>
-          <p>
-            I attended Broward College pursuing
-            my Bachelors Degree in Computer Science,
-            while attending I took a class that taught
-            me how to code Python... immediately I fell
-            in love with programming. I decided to give
-            this class my all and ended up with the
-            highest grade in the class. Once the semester
-            was over I decided that I was going to pursue
-            my passion of coding and go to a bootcamp.
-            I put my work, school, and life aside and
-            attended Ironhack Full time. Once I completed
-            my 9 weeks course at Ironhack I decided to
-            start on projects and volunteer at Make it
-            MVP to gain real life experience and work
-            on projects with other developers. I am
-            currently on the hunt for a position as a
-            web developer.
+          <div className="imgContainer">
+            <img className='aboutMeImage' src={aboutMeImg} />
+          </div>
+          <div className="text-box">
+            <p>
+              I attended Broward College pursuing
+              my Bachelors Degree in Computer Science,
+              while attending I took a class that taught
+              me how to code Python... immediately I fell
+              in love with programming. I decided to give
+              this class my all and ended up with the
+              highest grade in the class. Once the semester
+              was over I decided that I was going to pursue
+              my passion of coding and go to a bootcamp.
+              I put my work, school, and life aside and
+              attended Ironhack Full time. Once I completed
+              my 9 weeks course at Ironhack I decided to
+              start on projects and volunteer at Make it
+              MVP to gain real life experience and work
+              on projects with other developers. I am
+              currently on the hunt for a position as a
+              web developer.
           </p>
-          <button>View CV</button>
+            <button>View CV</button>
+          </div>
         </div>
       </section>
       {/* ---------ABOUT ME END--------- */}
 
       {/* ---------PROJECTS--------- */}
-      <h2>Latest Projects</h2>
-      <div className="accent"></div>
-      <div className='projects'>
-        <div className="projects-Box">
-          <h4>Dev Lane</h4>
-          <h6>Technologies</h6>
-          <i className="fab fa-github"></i>
+      <section className="project">
+        <h2>Latest Projects</h2>
+        <div className="accent"></div>
+        <div className='project-container'>
+          <div className="projects-Box">
+            <h4>Dev Lane</h4>
+            <h6>Technologies</h6>
+            <i className="fab fa-github"></i>
+          </div>
+          <div className="projects-Box">
+            <h4>Monopoly</h4>
+            <h6>Technologies</h6>
+            <i className="fab fa-github"></i>
+          </div>
+          <div className="projects-Box">
+            <h4>Juan B</h4>
+            <h6>Technologies</h6>
+            <i className="fab fa-github"></i>
+          </div>
+          <div className="projects-Box">
+            <h4>Bad Bunny</h4>
+            <h6>Technologies</h6>
+            <i className="fab fa-github"></i>
+          </div>
         </div>
-      </div>
+      </section>
 
       {/* ---------PROJECTS END--------- */}
 
