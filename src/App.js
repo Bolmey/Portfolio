3975c819fc1fafd9f240b3c43e7ca2710ae745f4
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-06-08 15:31:35 -0400

    email

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import headerImg from './img/JuanPort.png';
 import aboutMeImg from './img/juanabout.png';
+import emailjs from 'emailjs-com';
 
 
 function App() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'service_rvg5jju', e.target, 'user_b8UrQpDrr45ysmRw8Atjx')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset()
+  }
+
+
   return (
     <div className="App">
       {/* ---------NAV BAR--------- */}
@@ -145,7 +160,16 @@ function App() {
 
       {/* ---------CONTACT START--------- */}
       <section className="contactMe">
-
+        <form className="contact-form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </section>
 
 
